c48de6981d0b1b3778058c37266a10a1ef19ba5f
aganovia <aganovia@mail.gvsu.edu>
2021-04-08 19:38:04 -0400

    Server deleted, connection between 2 peers only

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 CIS457 Project 2 W2021
 
-A peer to peer application for text-based chat with emojis. Implemented in Python.
-
-TODO:
-
-- real-time message updating from all connected client windows
-- updated usernames in client windows
-- emoji support
+A peer to peer application for text-based chat between two peers. Implemented in Python.
---
+++ peer1.py
@@ -0,0 +1,101 @@
+import socket
+import sys
+import threading
+import tkinter
+import tkinter.scrolledtext
+from tkinter import simpledialog
+
+HOST = '127.0.0.1'
+PORT = 1234
+
+class Client:
+
+    def __init__(self, host, port):
+    
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((HOST, PORT))
+                
+        self.sock.listen()
+        peer, address = self.sock.accept() #FIXME? 
+        self.peer = peer
+        
+        msg = tkinter.Tk()
+        msg.withdraw()
+        
+        self.nickname = simpledialog.askstring("Nickname", "Please choose a nickname", parent=msg)
+        
+        self.gui_done = False
+        self.running = True
+        
+        gui_thread = threading.Thread(target=self.gui_loop)
+        receive_thread = threading.Thread(target=self.receive)
+        
+        gui_thread.start()
+        receive_thread.start()
+        
+    def gui_loop(self):
+        self.win = tkinter.Tk()
+        self.win.configure(bg="cyan")
+        
+        self.chat_label = tkinter.Label(self.win, text="Chat", bg="cyan")
+        self.chat_label.config(font=("Arial", 16))
+        self.chat_label.pack(padx=20, pady=5)
+        
+        self.text_area = tkinter.scrolledtext.ScrolledText(self.win, font=("Arial", 12))
+        self.text_area.pack(padx=20, pady=5)
+        self.text_area.config(state='disabled')
+        
+        self.msg_label = tkinter.Label(self.win, text="Message", bg="cyan")
+        self.msg_label.config(font=("Arial", 16))
+        self.msg_label.pack(padx=20, pady=5)
+        
+        self.input_area = tkinter.Text(self.win, height=3, font=("Arial", 12))
+        self.input_area.pack(padx=20, pady=5)
+        
+        self.send_button = tkinter.Button(self.win, text="Send", command=self.write)
+        self.send_button.config(font=("Arial", 16))
+        self.send_button.pack(padx=20, pady=5)
+        
+        self.gui_done = True
+        
+        self.win.protocol("WM_DELETE_WINDOW", self.stop)
+        
+        self.win.mainloop()
+        
+    def write(self):
+        message = f"{self.nickname}: {self.input_area.get('1.0', 'end')}"
+        self.peer.send(message.encode('utf-8'))
+        self.input_area.delete('1.0', 'end')
+        
+        self.text_area.config(state='normal')
+        self.text_area.insert('end', message)
+        self.text_area.yview('end')
+        self.text_area.config(state='disabled')
+        
+    def stop(self):
+        self.running = False
+        self.win.destroy()
+        self.peer.close()
+        exit(0)
+        
+    def receive(self):
+        while self.running:
+            try:
+                message = self.peer.recv(1024).decode('utf-8')
+                if message == 'NICK':
+                    self.peer.send(self.nickname.encode('utf-8'))
+                else:
+                    if self.gui_done:
+                        self.text_area.config(state='normal')
+                        self.text_area.insert('end', message)
+                        self.text_area.yview('end')
+                        self.text_area.config(state='disabled')
+            except ConnectionAbortedError:
+                break
+            except:
+                print("Error")
+                self.peer.close()
+                break
+                
+client = Client(HOST, PORT)
